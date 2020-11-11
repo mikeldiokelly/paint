17b11e973eb9929920637eaf3e6662a922b1868b
mikeldiokelly <47454996+mikeldiokelly@users.noreply.github.com>
2020-11-11 01:41:55 +0000

    Board takes top, left corner coordinates board status as inputs.

--- app/src/main/java/com/example/paint/GameActivity.java
+++ app/src/main/java/com/example/paint/GameActivity.java
@@ -9,25 +9,36 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.content.Intent;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 
 public class GameActivity extends AppCompatActivity {
 
-    private ImageButton[] buttons = new ImageButton[64];
-    private int[] color= new int[64];
+    private ImageButton[] buttons = new ImageButton[100];
+    private int[] color= new int[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 8; j++) {
                 String buttonID = "imgBtn_" + i + j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                buttons[8*i+j] = (ImageButton)findViewById(resID);
+                buttons[10*i+j] = (ImageButton)findViewById(resID);
             }
         }
-
+        //every T seconds, update the board
+        int[] start = new int[2];
+        int[] board = new int[16];
+        start [0] = 4;
+        start [1] = 4;
+        for (int i=0;i<16;i++){
+            board[i] = 0;
+        }
+        update_board(start,board);
         Button reset;
         reset=(Button) findViewById(R.id.reset);
         reset.setOnClickListener(this::onClick);
@@ -54,37 +65,42 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private  void resetarray(){
-        for (int i=0;i<8;i++){
+        for (int i=0;i<10;i++){
             for (int j=0;j<8;j++){
-                if ((i<4)&&(j<4)) {
-                    color[8*i+j] = 0;
-                    setboardcolor(i,j);
-                }
-                else {
-                    color[8*i+j] = 1;
-                    setboardcolor(i,j);
-                }
+                color[10 * i + j] = 1;
+                setboardcolor(i,j);
             }
         }
     }
 
+
     private void setboardcolor(int i,int j){
-
-        if(color[8*i+j]== 0) {
-//            buttons[i][j].setBackgroundColor(Color.parseColor("white"));
-//            buttons[i][j].setTextColor(Color.parseColor("white"));
-//            buttons[i][j].setText("x");
-            buttons[8*i+j].setImageResource(R.drawable.lightgreen);
-
+        switch(color[10*i+j]) {
+            case 0:
+                buttons[10*i+j].setImageResource(R.drawable.white);
+                break;
+            case 1:
+                buttons[10*i+j].setImageResource(R.drawable.lightgreen);
+                break;
+            case 2:
+                buttons[10*i+j].setImageResource(R.drawable.spalsh1blue);
+                break;
         }
-        else {
-//            buttons[i][j].setBackgroundColor(Color.parseColor("black"));
-//            buttons[i][j].setTextColor(Color.parseColor("black"));
-//            buttons[i][j].setText("0");
-            //buttons[i][j].setImageResource(R.drawable.splash3green);
-        }
-
     }
 
-
+    void update_board(int [] start, int [] board) {
+        resetarray();
+        int a=0;
+        for (int i = start[0]; i < start[0]+4; i++) {
+            for (int j = start[1]; j < start[1]+4; j++) {
+                color[10 * i + j] = board[a];
+                setboardcolor(i,j);
+                a++;
+            }
+        }
+    }
 }
+
+
+
+
